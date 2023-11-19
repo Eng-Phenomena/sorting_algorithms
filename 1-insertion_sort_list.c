327edc6ac07362bf08c0172e9010c2d1fb4e3685
Eng-Phenomena <eng.phenomena@gmail.com>
2023-11-19 11:48:01 -0800

    2

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,38 +7,38 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *tmp, *af, *pre;
+        listint_t *tmp, *af, *pre;
 
-	if (list && *list && (*list)->next)
-	{
-		tmp = (*list)->next;
-		af = tmp->next;
-		pre = tmp->prev;
+        if (list && *list && (*list)->next)
+        {
+                tmp = (*list)->next;
+                af = tmp->next;
+                pre = tmp->prev;
 
-		while (tmp)
-		{
-			if (tmp->n <  pre->n)
-			{
-				while (pre && tmp->n < pre->n)
-				{
-					if (af)
-						af->prev = pre;
-					tmp->next = pre;
-					tmp->prev = pre->prev;
-					if (pre->prev)
-						(pre->prev)->next = tmp;
-					pre->prev = tmp;
-					pre->next = af;
-					af = pre;
-					pre = tmp->prev;
-					if ((*list)->prev)
-						*list = (*list)->prev;
-					print_list(*list);
-				}
-			}
-			tmp = af;
-			af = tmp->next;
-			pre = tmp->prev;
-		}
-	}
+                while (tmp)
+                {
+                        while (pre && tmp->n < pre->n)
+                        {
+                                if (af)
+                                        af->prev = pre;
+                                tmp->next = pre;
+                                tmp->prev = pre->prev;
+                                if (pre->prev)
+                                        (pre->prev)->next = tmp;
+                                pre->prev = tmp;
+                                pre->next = af;
+                                af = pre;
+                                pre = tmp->prev;
+                                if ((*list)->prev)
+                                        *list = (*list)->prev;
+                                print_list(*list);
+                        }
+                        tmp = af;
+                        if (tmp)
+                        {
+                                af = tmp->next;
+                                pre = tmp->prev;
+                        }
+                }
+        }
 }
